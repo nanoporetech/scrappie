4a0a10949e97f3024b0c8ab4c99a0f64e9511156
Tim Massingham <tim.massingham@nanoporetech.com>
2016-11-23 14:11:47 +0000

    Replace LSTM file

--- parse_lstm.py
+++ parse_lstm.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
-
 import cPickle
 import numpy as np
 import sys
+
 
 model_file = sys.argv[1]
 
@@ -26,10 +26,20 @@ def cformatV(fh, name, X):
     # Mat object
     return '{} = mat_from_array({}, {}, {});\n'.format(name, '_' + name, X.shape[0], 1)
 
+
 def write_setup(fh, setup):
     fh.write('void setup(void){\n')
     fh.write(''.join(map(lambda x: '\t' + x, setup)))
     fh.write('}\n')
+
+
+def reshape_lstmM(mat):
+	_, isize = mat.shape
+	return mat.reshape((-1, 4, isize)).transpose([1, 0, 2]).reshape((-1, isize))
+
+
+def reshape_lstmV(mat):
+	return mat.reshape((-1, 4)).transpose().reshape(-1)
 
 
 with open(model_file, 'r') as fh:
@@ -38,33 +48,32 @@ with open(model_file, 'r') as fh:
 sys.stdout.write("""#ifndef NANONET_MODEL_H
 #define NANONET_MODEL_H
 #include "util.h"
-
 """)
 
 """ First GRU layer
 """
 setup = []
 
-bigru1 = network.layers[1]
-gru = bigru1.layers[0]
+bilstm1 = network.layers[1]
+lstm = bilstm1.layers[0]
 setup.append(
-	cformatM(sys.stdout, 'gruF1_iW', gru.iW.get_value()))
+	cformatM(sys.stdout, 'lstmF1_iW', reshape_lstmM(lstm.iW.get_value())))
 setup.append(
-	cformatM(sys.stdout, 'gruF1_sW', gru.sW.get_value()))
+	cformatM(sys.stdout, 'lstmF1_sW', reshape_lstmM(lstm.sW.get_value())))
 setup.append(
-	cformatM(sys.stdout, 'gruF1_sW2', gru.sW2.get_value()))
+	cformatV(sys.stdout, 'lstmF1_b', reshape_lstmV(lstm.b.get_value().reshape(-1))))
 setup.append(
-	cformatV(sys.stdout, 'gruF1_b', gru.b.get_value().reshape(-1)))
+	cformatV(sys.stdout, 'lstmF1_p', lstm.p.get_value().reshape(-1)))
 
-gru = bigru1.layers[1].layer
+lstm = bilstm1.layers[1].layer
 setup.append(
-	cformatM(sys.stdout, 'gruB1_iW', gru.iW.get_value()))
+	cformatM(sys.stdout, 'lstmB1_iW', reshape_lstmM(lstm.iW.get_value())))
 setup.append(
-	cformatM(sys.stdout, 'gruB1_sW', gru.sW.get_value()))
+	cformatM(sys.stdout, 'lstmB1_sW', reshape_lstmM(lstm.sW.get_value())))
 setup.append(
-	cformatM(sys.stdout, 'gruB1_sW2', gru.sW2.get_value()))
+	cformatV(sys.stdout, 'lstmB1_b', reshape_lstmV(lstm.b.get_value().reshape(-1))))
 setup.append(
-	cformatV(sys.stdout, 'gruB1_b', gru.b.get_value().reshape(-1)))
+	cformatV(sys.stdout, 'lstmB1_p', lstm.p.get_value().reshape(-1)))
 
 
 """ First feed forward layer
@@ -80,26 +89,26 @@ setup.append(
 
 """ Second GRU layer
 """
-bigru2 = network.layers[3]
-gru = bigru2.layers[0]
+bilstm2 = network.layers[3]
+lstm = bilstm2.layers[0]
 setup.append(
-	cformatM(sys.stdout, 'gruF2_iW', gru.iW.get_value()))
+	cformatM(sys.stdout, 'lstmF2_iW', reshape_lstmM(lstm.iW.get_value())))
 setup.append(
-	cformatM(sys.stdout, 'gruF2_sW', gru.sW.get_value()))
+	cformatM(sys.stdout, 'lstmF2_sW', reshape_lstmM(lstm.sW.get_value())))
 setup.append(
-	cformatM(sys.stdout, 'gruF2_sW2', gru.sW2.get_value()))
+	cformatV(sys.stdout, 'lstmF2_b', reshape_lstmV(lstm.b.get_value().reshape(-1))))
 setup.append(
-	cformatV(sys.stdout, 'gruF2_b', gru.b.get_value().reshape(-1)))
+	cformatV(sys.stdout, 'lstmF2_p', lstm.p.get_value().reshape(-1)))
 
-gru = bigru2.layers[1].layer
+lstm = bilstm2.layers[1].layer
 setup.append(
-	cformatM(sys.stdout, 'gruB2_iW', gru.iW.get_value()))
+	cformatM(sys.stdout, 'lstmB2_iW', reshape_lstmM(lstm.iW.get_value())))
 setup.append(
-	cformatM(sys.stdout, 'gruB2_sW', gru.sW.get_value()))
+	cformatM(sys.stdout, 'lstmB2_sW', reshape_lstmM(lstm.sW.get_value())))
 setup.append(
-	cformatM(sys.stdout, 'gruB2_sW2', gru.sW2.get_value()))
+	cformatV(sys.stdout, 'lstmB2_b', reshape_lstmV(lstm.b.get_value().reshape(-1))))
 setup.append(
-	cformatV(sys.stdout, 'gruB2_b', gru.b.get_value().reshape(-1)))
+	cformatV(sys.stdout, 'lstmB2_p', lstm.p.get_value().reshape(-1)))
 
 
 """ Second feed forward layer
@@ -111,6 +120,7 @@ setup.append(
 	cformatM(sys.stdout, 'FF2_Wb', network.layers[4].W.get_value()[:, size : 2 * size]))
 setup.append(
 	cformatV(sys.stdout, 'FF2_b', network.layers[4].b.get_value()))
+
 
 """ Softmax layer
 """
