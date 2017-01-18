eab86fe071f72cee39ce201754bce572826e1e23
Tim Massingham <tim.massingham@nanoporetech.com>
2017-01-18 14:25:06 +0000

    Scripts for homopolymer correction

--- misc/homo.py
+++ misc/homo.py
@@ -6,6 +6,7 @@ from untangled.bio import all_kmers, kmer_mapping
 from untangled.cmdargs import Positive
 
 parser = argparse.ArgumentParser()
+parser.add_argument('--factor', default=[0, 9.5], nargs=2, type=float)
 parser.add_argument('--kmer', default=5, type=Positive(int))
 parser.add_argument('hdf5')
 
@@ -23,43 +24,45 @@ if __name__ == '__main__':
             events = events[events['pos'] >= 0]
             nev = len(events)
 
-            for base in ['A', 'C', 'G', 'T']:
-                homostate = 1 + kmer_to_state[base * args.kmer]
+            homostates = {1 + kmer_to_state[b * args.kmer] : b for b in ['A', 'C', 'G', 'T']}
 
-                ev = 0
-                while ev < nev:
-                    while ev < nev and events['state'][ev] != homostate:
-                        #  Increment ev until next homopolymer found
-                        ev += 1
-                    if ev == nev:
+            ev = 0
+            while ev < nev:
+                while ev < nev and events['state'][ev] not in homostates:
+                    #  Increment ev until next homopolymer found
+                    ev += 1
+                if ev == nev:
+                    break
+
+                hbase = homostates[events['state'][ev]]
+
+                hstart, hend = ev, ev + 1
+                for evi in range(hstart, 0, -1):
+                    # Track back until find start.
+                    evst = events['state'][evi - 1]
+                    if evst == 0:
+                        continue
+                    #print "ev {} has kmer {}".format(evi - 1, kmer[evst -1 ])
+                    if kmer[evst - 1][kidx] != hbase:
+                        break
+                    hstart = evi - 1
+                for evi in range(ev + 1, nev):
+                    # Track forward until find end.
+                    evst = events['state'][evi]
+                    hend = evi + 1
+                    if evst == 0:
+                        continue
+                    #print "ev {} has kmer {} ({})".format(evi, kmer[evst - 1], kmer[evst - 1][kidx])
+                    if kmer[evst - 1][kidx] != hbase:
+                        hend = evi
+                        #print '... break' 
                         break
 
-                    hstart, hend = ev, ev + 1
-                    for evi in range(hstart, 0, -1):
-                        # Track back until find start.
-                        evst = events['state'][evi - 1]
-                        if evst == 0:
-                            continue
-                        #print "ev {} has kmer {}".format(evi - 1, kmer[evst -1 ])
-                        if kmer[evst - 1][kidx] != base:
-                            break
-                        hstart = evi - 1
-                    for evi in range(ev + 1, nev):
-                        # Track forward until find end.
-                        evst = events['state'][evi]
-                        hend = evi + 1
-                        if evst == 0:
-                            continue
-                        #print "ev {} has kmer {} ({})".format(evi, kmer[evst - 1], kmer[evst - 1][kidx])
-                        if kmer[evst - 1][kidx] != base:
-                            hend = evi
-                            #print '... break' 
-                            break
+                print read, hbase, hstart, hend - 1,
+                dwell = np.sum(events['length'][hstart : hend])
+                corrected_dwell = round((dwell - args.factor[0]) / args.factor[1])
+                hl = events['pos'][hend - 1] - events['pos'][hstart] + 1
+                #print events[hstart:hend]
+                print events['pos'][hstart], hl, dwell, corrected_dwell
 
-                    print read, base, hstart, hend - 1,
-                    dwell = np.sum(events['length'][hstart : hend])
-                    hl = events['pos'][hend - 1] - events['pos'][hstart] + 1
-                    #print events[hstart:hend]
-                    print events['pos'][hstart], hl, dwell
-
-                    ev = hend
+                ev = hend
