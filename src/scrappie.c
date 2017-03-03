5059963603bf2630cf1e8e91db59b39bd7450a23
Tim Massingham <tim.massingham@nanoporetech.com>
2017-03-03 10:08:40 +0000

    Allow Scrappie to read event from files produced from Albacore

--- src/scrappie.c
+++ src/scrappie.c
@@ -50,6 +50,8 @@ static struct argp_option options[] = {
 	{"no-slip", 2, 0, OPTION_ALIAS, "Disable slipping"},
         {"segmentation", 3, "group", 0, "Fast5 group from which to reads segmentation"},
 	{"dump", 4, "filename", 0, "Dump annotated events to HDF5 file"},
+	{"albacore", 7, 0, 0, "Assume fast5 have been called using Albacore"},
+	{"no-albacore", 8, 0, OPTION_ALIAS, "Assume fast5 have been called using Albacore"},
 #if defined(_OPENMP)
 	{"threads", '#', "nreads", 0, "Number of reads to call in parallel"},
 #endif
@@ -70,8 +72,9 @@ struct arguments {
 	char * segmentation;
 	char * dump;
 	char ** files;
+	bool albacore;
 };
-static struct arguments args = {0, true, 0, 1e-5, FORMAT_FASTA, 0.0, false, 50, "Segment_Linear", NULL};
+static struct arguments args = {0, true, 0, 1e-5, FORMAT_FASTA, 0.0, false, 50, "Segment_Linear", NULL, false};
 
 
 static error_t parse_arg(int key, char * arg, struct  argp_state * state){
@@ -123,6 +126,12 @@ static error_t parse_arg(int key, char * arg, struct  argp_state * state){
 	case 6:
 		args.dwell_correction = false;
 		break;
+	case 7:
+		args.albacore = true;
+		break;
+	case 8:
+		args.albacore = false;
+		break;
 
 	#if defined(_OPENMP)
 	case '#':
@@ -167,7 +176,11 @@ char * kmer_from_state(int state, int klen, char * kmer){
 
 struct _bs calculate_post(char * filename){
 	const int WINLEN = 3;
-	event_table et = read_detected_events(filename, args.analysis, args.segmentation);
+
+	event_table et = args.albacore ?
+		read_albacore_events(filename, args.analysis, "template") :
+		read_detected_events(filename, args.analysis, args.segmentation);
+
 	if(NULL == et.event){
 		return (struct _bs){0, 0, NULL};
 	}
