f44dc66c52a22e16bc0cb6afa07981dc2dc6a961
Tim Massingham <tim.massingham@nanoporetech.com>
2017-06-12 15:31:08 +0100

    Update parsers to Python3

--- misc/parse_events.py
+++ misc/parse_events.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python
-import cPickle
+#!/usr/bin/env python3
+import pickle
 import math
 import numpy as np
 import sys
@@ -44,8 +44,8 @@ def reshape_lstmV(mat):
 	return mat.reshape((-1, 4)).transpose().reshape(-1)
 
 
-with open(model_file, 'r') as fh:
-    network = cPickle.load(fh)
+with open(model_file, 'rb') as fh:
+    network = pickle.load(fh, encoding='latin1')
 
 sys.stdout.write("""#ifndef NANONET_EVENTS_MODEL_H
 #define NANONET_EVENTS_MODEL_H
